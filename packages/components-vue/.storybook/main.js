f66bfe79dec052de1cc4865de6cc5e40b776f59c
zkp442910864 <442910864@qq.com>
2021-07-04 22:46:11 +0800

    demo

--- packages/components-vue/.storybook/main.js
+++ packages/components-vue/.storybook/main.js
@@ -2,8 +2,8 @@ module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
-	"../../common-docs/react-demo/**/*.stories.mdx",
-    "../../common-docs/react-demo/**/*.stories.@(js|jsx|ts|tsx)",
+    "../../common-docs/vue-demo/**/*.stories.mdx",
+    "../../common-docs/vue-demo/**/*.stories.@(js|jsx|ts|tsx)",
     "../../common-docs/common-mdx/**/*.stories.mdx",
   ],
   "addons": [
